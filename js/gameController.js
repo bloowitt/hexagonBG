3840f8238b5c67ede777b1af397bdcc94a8b382f
Alex OdG <bloowitt@gmail.com>
2015-06-21 17:48:58 +0200

    hexagon sizing, pan on the board

--- js/gameController.js
+++ js/gameController.js
@@ -14,6 +14,25 @@ window.hexagonBG.gameController = (function( element ){
 	$(window).on("resize", function(event){
 		hexagonBG.gameController.recalcCanvasSize();
 		hexagonBG.gameController.recalcHexSizes();
+		hexagonBG.gameController.redraw();
+	});
+
+	// Listen for events
+	$(window).on("keydown", function(event){
+		switch (event.key){
+			case "Down":
+				hexagonBG.gameController.panDown();
+				break;
+			case "Up":
+				hexagonBG.gameController.panUp();
+				break;
+			case "Left":
+				hexagonBG.gameController.panLeft();
+				break;
+			case "Right":
+				hexagonBG.gameController.panRight();
+				break;
+		}
 	});
 
 	// Game constants
@@ -25,9 +44,11 @@ window.hexagonBG.gameController = (function( element ){
 	// Game status
 	var curColumns = 20;
  	var A, B, C, curHexSize;
+ 	var boardSize = 100;
+ 	var viewCenterX = Math.floor(boardSize/2);
+ 	var viewCenterY = Math.floor(boardSize/2);
 
 	// Public methods
-
 	var recalcCanvasSize = function(){
 		var windowWidth 	= window.innerWidth 	- (2*FRAMESIZE)
 		var windowHeight 	= window.innerHeight 	- (2*FRAMESIZE)
@@ -44,32 +65,78 @@ window.hexagonBG.gameController = (function( element ){
 	}
 
 	var recalcHexSizes = function(){
-		C = $canvas.width / (curColumns+1)
+		// 1.5 seems to be the magic number that makes this work correctly
+		C = $canvas.width / (curColumns*1.5)
 		A = C*0.5;
 		B = C*Math.sin((2*Math.PI) / 6);
 	}
 
+	var panLeft = function(){
+		if (viewCenterX - curColumns/2 > 0){
+			viewCenterX = viewCenterX -1;
+		} else {
+			// TODO show pan limit
+
+		}
+		redraw();
+	}
+
+	var panRight = function(){
+		if (viewCenterX + curColumns/2 < boardSize){
+			viewCenterX = viewCenterX +1;
+		} else {
+			// TODO show pan limit
+
+		}
+		redraw();
+	}
+
+	var panUp = function(){
+		if (viewCenterY - (curColumns/(ASPECTRATIO*2)) > 0){
+			viewCenterY = viewCenterY -1;
+		} else {
+			// TODO show pan limit
+
+		}
+		redraw();
+	}
+
+	var panDown = function(){
+	if (viewCenterY + (curColumns/(2*ASPECTRATIO)) < boardSize){
+			viewCenterY = viewCenterY +1;
+		} else {
+			// TODO show pan limit
+
+		}
+		redraw();
+	}
+
+	var zoom = function(zoomIn){
+
+	}
 	// Force this on load
 	recalcCanvasSize();
 	recalcHexSizes();
 
 	// "Private" methods
 	var redraw = function() {
+		console.log(viewCenterX + "," + viewCenterY)
 		// Preparation		
 		ctx.save();
-		// Clear the screen (should replace with dirtyRects sometime TODO)
-		ctx.clearRect(0,0,canvasWidth,canvasHeight);
-
+		
 		ctx.lineWidth = 1;
 		ctx.strokeStyle = "rgb(255,255,255)";
 		ctx.fillStyle = "rgb(255,255,255)";
 
-		// Draw hexagons
+		// Clear the screen (should replace with dirtyRects sometime TODO)
+		ctx.clearRect(0,0,canvasWidth,canvasHeight);
 		hexagonBG.boardDraw.drawHexagonGrid(ctx,canvasWidth,canvasHeight,A,B,C);
-		
+			
+
+		// Draw game elements
+
 		// Cleanup and setup next frame
 		ctx.restore();
-		requestAnimationFrame(redraw);
 	}
 
 
@@ -79,6 +146,11 @@ window.hexagonBG.gameController = (function( element ){
 
 	return {
 		recalcCanvasSize: recalcCanvasSize,
-		recalcHexSizes: recalcHexSizes
+		recalcHexSizes: recalcHexSizes,
+		redraw: redraw,
+		panLeft: panLeft,
+		panRight: panRight,
+		panUp: panUp,
+		panDown: panDown
 	};
 }) ();
